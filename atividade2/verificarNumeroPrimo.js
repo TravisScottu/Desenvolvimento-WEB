3004298edabaeff2e1e4db2ed26b3b150f4ead6e
Your Name <you@example.com>
2025-03-17 14:09:48 -0300

    commit

--- atividade2/verificarNumeroPrimo.js
+++ atividade2/verificarNumeroPrimo.js
@@ -7,19 +7,16 @@ function verificarNumeroPrimo(n) {
         return true;
     }
 
-    
     if (n % 2 === 0 || n % 3 === 0) {
         return false;
     }
-
-    
+ 
     for (let i = 5; i * i <= n; i += 6) {
         if (n % i === 0 || n % (i + 2) === 0) {
             return false;
         }
     }
 
-    
     return true;
 }
 console.log(verificarNumeroPrimo(0));
