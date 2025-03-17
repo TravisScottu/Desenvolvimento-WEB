fd5de373d1f23beec6d2e172f161dbdc96e6dc63
Your Name <you@example.com>
2025-03-17 13:08:17 -0300

    ini

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+let numA1 = 10;
+let numB1 = 5;
+let resultadoAdicao = numA1 + numB1;
+console.log(`Resultado da adição entre ${numA1} e ${numB1} é ${resultadoAdicao}`);
+
+let numA2 = 20;
+let numB2 = 7;
+let resultadoSubtracao = numA2 - numB2;
+console.log(`Resultado da subtração entre ${numA2} e ${numB2} é ${resultadoSubtracao}`);
+
+let numA3 = 6;
+let numB3 = 4;
+let resultadoMultiplicacao = numA3 * numB3;
+console.log(`Resultado da multiplicação entre ${numA3} e ${numB3} é ${resultadoMultiplicacao}`);
+
+let numA4 = 15;
+let numB4 = 3;
+let resultadoDivisao = numA4 / numB4;
+console.log(`Resultado da divisão entre ${numA4} e ${numB4} é ${resultadoDivisao}`);
